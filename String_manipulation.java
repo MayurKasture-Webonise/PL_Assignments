d8831e50a5ce139ab37427567ec570b7f43e9cc6
MayurKasture-Webonise <mayur.kasture@weboniselab.com>
2021-08-23 20:14:14 +0530

    Additional Changes

--- String_manipulation.java
+++ String_manipulation.java
@@ -1,3 +1,34 @@
 // Pl 1st Assignment.
 
+import java.util.Scanner;
 
+class String_manipulation
+{
+    Scanner sc = new Scanner(System.in);
+    int i;
+
+    void task()
+    {
+        System.out.print("Enter Your String : ");
+        String str = sc.nextLine();
+
+        System.out.println(str);
+        while(str.length() > 1)
+        {
+            String new_line="";
+            for(i=0;i<=str.length()-1; i++)
+            {
+                new_line = new_line + str.charAt(i);
+                i= i+1;
+            }
+            System.out.println(new_line);
+            str = new_line;
+        }
+    }
+
+    public static void main(String args[])
+    {
+        String_manipulation obj = new String_manipulation();
+        obj.task();
+    }
+}
